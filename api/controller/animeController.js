578adae31b929b541c920e94436c012449a4047e
nick0205-br <nicolasdlopes@gmail.com>
2022-06-06 17:25:48 -0300

    4

--- api/controller/animeController.js
+++ api/controller/animeController.js
@@ -5,7 +5,22 @@ import { Router } from 'express'
 const server = Router();
 
 
-server.post('/anime', async (req, resp) () => {
+server.post('/anime', async (req, resp) => {
+    try{
+        const inserirAnime = req.body;
+        const anime = await inserirAnime (inserirAnime);
+        resp.send({
+            anime
+        });
+    }
+    catch(err){
+        resp.status(404).send({
+            erro: err.message
+        })
+    }
+})
+
+server.get('/consulta', async (req, resp) => {
     try{
         const animeinserir = req.body;
         const anime = await inserirAnime (inserirAnime);
--- api/repository/repository.js
+++ api/repository/repository.js
@@ -5,7 +5,7 @@ export async function inserirAnime(anime){
     const comando = 'INSERT INTO TB_ANIME (id_anime, nm_anime)'
                                   VALUES (1, 'Naruto')
 
-    const [resposta] = await con.query (comando, [anime.nome, anime.usuario, anime.genero]), anime, id = resposta, insertID;
+    const [resposta] = await con.query (comando, [anime.nome, anime.id]), anime = resposta;
 
     return anime;
 }
