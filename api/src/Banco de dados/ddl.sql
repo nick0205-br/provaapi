55aaccc10c17f446eed83ef96c4f1d4f3c853d58
nick0205-br <nicolasdlopes@gmail.com>
2022-06-06 16:29:43 -0300

    oi

---
+++ api/src/Banco de dados/ddl.sql
@@ -0,0 +1,7 @@
+CREATE database PROVA;
+USE PROVA;
+CREATE TABLE TB_ANIME(
+		id_anime		int,
+        nm_anime		varchar(50)
+);
+
---
+++ api/src/Banco de dados/dml.sql
@@ -0,0 +1,9 @@
+USE roses;
+
+
+-- CSUO 01 Consultar
+SELECT TB_ANIME;
+
+-- CSUO 02 Add informações
+INSERT INTO TB_ANIME(nm_anime)
+        ('?');
