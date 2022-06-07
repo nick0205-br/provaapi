bddb155158ed88640455ec6b8d21fc2bc1dd0cda
nick0205-br <nicolasdlopes@gmail.com>
2022-06-06 21:50:13 -0300

    7

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import controller from './controller/controller.js'
+import controller from '../controller/animecontroller.js'
 
 const server = express();
 server.use(express.json());
 server.use(cors());
-server.use(animecontroler());
+server.use(controller());
 
-server.listen(process.env.PORT, 
+
+server.listen(process.env.PORT,
               () => console.log(`API online na porta ${process.env.PORT}`));
