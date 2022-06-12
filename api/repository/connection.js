e9ac11094d9484a0eb30459f16582cd8feee8a01
nick0205-br <82313802+nick0205-br@users.noreply.github.com>
2022-06-12 19:36:44 -0300

    Update connection.js

--- api/repository/connection.js
+++ api/repository/connection.js
@@ -1,12 +1,12 @@
 import mysql from 'mysql2/promise'
 
 const con = await mysql.createconnection({
-    host:process.env.MYSQL_HOST,
-    user:process.env.MYSQL_USER,
-    password:process.env.MYSQL_PWD,
-    database:process.env.MYSQL_DB
-})
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PWD,
+    database: process.env.MYSQL_DB
+});
 
-console.log('DB conected')
+console.log('DB conected');
 
 export { con }
