f86498662a6b348c519f89bf1c03262546bda097
nick0205-br <82313802+nick0205-br@users.noreply.github.com>
2022-06-12 19:31:58 -0300

    Update dml.sql

--- api/Banco de dados/dml.sql
+++ api/Banco de dados/dml.sql
@@ -2,8 +2,9 @@ USE PROVA;
 
 
 -- CSUO 01 Consultar
-SELECT * FROM TB_ANIME;
+SELECT * FROM TB_ANIME
+        WHERE id_anime = 0;
 
 -- CSUO 02 Add informações
-INSERT INTO TB_ANIME(id_anime, nm_anime)
-    VALUES (1, '?');
+INSERT INTO TB_ANIME(nm_anime)
+    VALUES ( '?');
