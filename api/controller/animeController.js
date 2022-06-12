05e6d8996c091f06b449bcade6f590204673679d
nick0205-br <82313802+nick0205-br@users.noreply.github.com>
2022-06-12 19:33:26 -0300

    Update animeController.js

--- api/controller/animeController.js
+++ api/controller/animeController.js
@@ -1,36 +1,25 @@
-import { inserirAnime, consultarAnime } from '../repository/animerepository.js'
+import { consultarAnimesPorID, inserirAnime } from "../Repository/animeRepository.js";
 import { Router } from 'express'
-
-
 const server = Router();
 
-
-server.post('/anime', async (req, resp) => {
+server.post('/anime/', async (req, resp) => {
     try{
-        const animeinserir = req.body;
-        
-        const anime = await inserirAnime (animeinserir);
-        
-        resp.send({
-            anime: anime
-        });
-    }
-    catch(err){
+        const { animes } = req.body;
+        const animeInserido = await inserirAnime(animes);
+        resp.send(animeInserido);
+    } catch(err) {
         resp.status(404).send({
             erro: err.message
         })
     }
 })
 
-server.get('/anime', async (req, resp) => {
+server.get('/consulta/:id', async (req, resp) => {
     try{
-        const re = await consultarAnime()
-
-        resp.send({
-            anime: re
-        })
-    }
-    catch(err){
+        const id = req.params.id;
+        const consultaAnime = await consultarAnimesPorID(id)
+        resp.send(consultaAnime);
+    } catch(err) {
         resp.status(404).send({
             erro: err.message
         })
